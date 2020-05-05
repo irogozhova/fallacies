7da8205b9751c9aa48c6de2cfe9fdbe5f1e8446f
Irina Rogozhova <irina.rogozhova@saltpepper.ru>
2020-05-05 11:45:02 +0300

    Fixed positioning of cards

--- src/components/App.styled.ts
+++ src/components/App.styled.ts
@@ -3,4 +3,5 @@ import styled from "styled-components";
 export const Root = styled.div`
   display: flex;
   flex-wrap: wrap;
+  justify-content: space-evenly;
 `;
--- src/components/Card/Card.styled.ts
+++ src/components/Card/Card.styled.ts
@@ -2,6 +2,5 @@ import styled from "styled-components";
 
 export const Root = styled.div`
   margin-bottom: 20px;
-  margin-right: 20px;
 `;
 
