7e3aabf87c191570976ecc70134ebc89969df3d9
Irina Rogozhova <irina.rogozhova@saltpepper.ru>
2020-05-05 20:43:46 +0300

    Updates

---
+++ precache-manifest.2a379a186657e86d6710ae3187e05ad6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "435a86bac98b7fbaab069c3b8799db76",
+    "url": "/fallacies/index.html"
+  },
+  {
+    "revision": "fc1fa6fd6c7c1396dc0a",
+    "url": "/fallacies/static/js/2.bed14eb0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fallacies/static/js/2.bed14eb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ef9de71d581a5936501",
+    "url": "/fallacies/static/js/main.3a1240e6.chunk.js"
+  },
+  {
+    "revision": "bb4b7bf119ee9f08f74d",
+    "url": "/fallacies/static/js/runtime-main.1fd54a81.js"
+  },
+  {
+    "revision": "f3ebe3466dc0b223320d882143c4decd",
+    "url": "/fallacies/static/media/dice.f3ebe346.svg"
+  }
+]);
