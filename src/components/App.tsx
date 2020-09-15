8176d4ca2735642fb871a4128ad3c7e50719cff3
Irina Rogozhova <irina.rogozhova@saltpepper.ru>
2020-09-15 21:33:35 +0300

    Prettified one file

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,16 @@
 import React, { useState, useCallback } from "react";
-import Card from './Card';
-import CardSide from './Card/CardSide';
-import Modal from './Modal';
-import dice from './dice.svg';
+import Card from "./Card";
+import CardSide from "./Card/CardSide";
+import Modal from "./Modal";
+import dice from "./dice.svg";
 
 import * as Styled from "./App.styled";
 
-import cards from './cards.json';
+import cards from "./cards.json";
 
 const generateCardNumbers = () => {
   const arr = [];
-  while(arr.length < 5){
+  while (arr.length < 5) {
     let r = Math.floor(Math.random() * 44);
     if (arr.indexOf(r) === -1) {
       arr.push(r);
@@ -18,24 +18,26 @@ const generateCardNumbers = () => {
   }
 
   return arr.sort((a, b) => a - b);
-}
+};
 
 const App = () => {
   const [cardIndexes, setCardIndexes] = useState<number[] | null>(null);
-  const [selectedCardIndex, setSelectedCardIndex] = useState<number | null>(null);
+  const [selectedCardIndex, setSelectedCardIndex] = useState<number | null>(
+    null
+  );
 
   const handleClick = useCallback((e) => {
     e.preventDefault();
     setCardIndexes(generateCardNumbers());
-  },[]);
+  }, []);
 
   const handleMouseOver = useCallback((item) => {
     setSelectedCardIndex(item);
-  },[]);
+  }, []);
 
   const handleMouseOut = useCallback(() => {
     setSelectedCardIndex(null);
-  },[]);
+  }, []);
 
   return (
     <Styled.Root>
@@ -56,32 +58,28 @@ const App = () => {
         <Styled.PlaceHolder />
         <Styled.PlaceHolder />
       </Styled.Cards>
-      <Styled.GenerateBtn
-        onClick={handleClick}
-        disabled={cardIndexes !== null}
-      >
+      <Styled.GenerateBtn onClick={handleClick} disabled={cardIndexes !== null}>
         {cardIndexes === null ? (
           <>
             Get 5 random cards
             <img src={dice} alt="dice" />
           </>
-          ) : (
-            <Styled.Numbers>
-              Your cards:
-              {cardIndexes.map((item, i) => (
-                <div
-                  key={i}
-                  onMouseOver={() => handleMouseOver(item)}
-                  onMouseOut={handleMouseOut}
-                >
-                  <span>{item}</span>
-                </div>
-              ))}
-            </Styled.Numbers>
-          )
-        }
+        ) : (
+          <Styled.Numbers>
+            Your cards:
+            {cardIndexes.map((item, i) => (
+              <div
+                key={i}
+                onMouseOver={() => handleMouseOver(item)}
+                onMouseOut={handleMouseOut}
+              >
+                <span>{item}</span>
+              </div>
+            ))}
+          </Styled.Numbers>
+        )}
       </Styled.GenerateBtn>
-      {selectedCardIndex !== null &&
+      {selectedCardIndex !== null && (
         <Modal>
           <CardSide
             index={selectedCardIndex}
@@ -94,9 +92,9 @@ const App = () => {
             content={cards[selectedCardIndex].ruCard}
           />
         </Modal>
-      }
+      )}
     </Styled.Root>
   );
-}
+};
 
 export default App;
